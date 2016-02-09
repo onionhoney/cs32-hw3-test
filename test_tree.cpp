25342d0062674a830dab78a06bd949593cb66445
Xinyu (Colin) Yang <colinxy@users.noreply.github.com>
2016-02-08 16:19:10 -0800

    fix code style

--- test_tree.cpp
+++ test_tree.cpp
@@ -24,7 +24,7 @@ bool test_includes(int testIndex) {
     double b2[] {10, 40, 30};
     double b3[] {20, 10, 40};
     double b4[] {50, 40, 30};
-    
+
     switch (testIndex) {
         case 0:
             return check(countIncludes(a, 7, b1, 3) == 1);
@@ -37,12 +37,16 @@ bool test_includes(int testIndex) {
             
         case 3:
             return check(countIncludes(a, 7, b4, 3) == 3);
+
         case 4:
             return check(countIncludes(a, 7, b1, 0) == 1);
+
         case 5:
             return check(countIncludes(a, 0, b1, 0) == 1);
+
         case 6:
             return check(countIncludes(a, 0, b1, 3) == 0);
+
         default:
             return false;
     }
@@ -53,16 +57,16 @@ bool test_order() {
     double a[] {10, 50, 40, 20, 50, 40, 30};
     double b[] {10, 20, 30};
     //bondary test
-    cout << "=== RUNNING BONDARY TEST FOR order ===" << endl;
+    cout << "=== RUNNING BOUNDARY TEST FOR order ===" << endl;
     order(b, 0);
     if(b[0] == 10 && b[1] == 20 && b[2] == 30)
         cout << "PASSED BONDARY TEST" << endl;
     else{
         cout << "*** FAILURE ***" << endl
-        << "fails bondary test" << endl;
+        << "fails boundary test" << endl;
     }
-    cout << "=== END OF BONDARY TEST FOR order ===" << endl;
-    
+    cout << "=== END OF BOUNDARY TEST FOR order ===" << endl;
+
     // trivial test
     cout << "=== RUNNING TRIVIAL TEST FOR order ===" << endl;
     
@@ -72,7 +76,7 @@ bool test_order() {
     for (int i = 0; i < 7; ++i) {
         if (a[i] > last) {
             cout << "*** FAILURE ***" << endl
-            << "fails trivial test" << endl;
+                 << "fails trivial test" << endl;
             trivial_passed = false;
             break;
         }
@@ -81,7 +85,7 @@ bool test_order() {
     if (trivial_passed)
         cout << "PASSED TRIVIAL TEST" << endl;
     cout << "=== END OF TRIVIAL TEST FOR order ===" << endl;
-    
+
     //randomized test
     cout << "=== RUNNING RANDOMIZED TEST FOR order ===" << endl;
     const int SIZE = 100000;
@@ -90,14 +94,14 @@ bool test_order() {
     for (int i = 0; i < SIZE; ++i) {
         d_arr[i] = rand();
     }
-    
+
     bool randomized_passed = true;
     order(d_arr, SIZE);
     last = d_arr[0];
     for (int i = 0; i < SIZE; ++i) {
         if (d_arr[i] > last) {
             cout << "*** FAILURE ***" << endl
-            << "fails randomized test" << endl;
+                 << "fails randomized test" << endl;
             randomized_passed = false;
             break;
         }
@@ -106,29 +110,30 @@ bool test_order() {
     if (randomized_passed)
         cout << "PASSED RANDOMIZED TEST" << endl;
     cout << "=== END OF RANDOMIZED TEST FOR order ===" << endl;
-    
+
     return true;
 }
 
 
 int main() {
     cout << "=== TESTING countIncludes ===" << endl;
-    
+
     int countIncludes_passed = 0;
     for (int i = 0; i < TOTAL_INCLUDES; ++i) {
         if (!test_includes(i))
             cout << "*** FAILURE ***" << endl
-            << "fails test case " << i << endl
+                 << "fails test case " << i << endl
             << message << endl;
         else
             ++countIncludes_passed;
     }
-    
+
     cout << "PASSED " << countIncludes_passed
-    << " OUT OF " << TOTAL_INCLUDES << endl;
+         << " OUT OF " << TOTAL_INCLUDES << endl;
     cout << "=== END OF countIncludes ===" << endl << endl;
-    
+
+    // test order function
     test_order();
-    
+
     return 0;
 }
